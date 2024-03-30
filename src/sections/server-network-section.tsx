63dcabf68b8b0f50ce3bbc7ffada0d94094cabe7
Mirasaki <pcr.hillebrand1998@gmail.com>
2024-03-30 14:12:05 +0100

    fix: use span element instead of div inside paragraph

--- src/sections/server-network-section.tsx
+++ src/sections/server-network-section.tsx
@@ -76,7 +76,7 @@ export const ServerCard = async ({
         </h3>
       </div>
       <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
-        {data?.error && <div className='text-red-500'>Error: {data.error}</div>}
+        {data?.error && <span className='text-red-500'>Error: {data.error}</span>}
         {!data?.error && <>
           Players: {serverData?.players ?? 0}/{serverData?.max_players ?? 0}
           <br />
